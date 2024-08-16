091f993000255bc28150bfe989b2a6f285fdd50a
noneda <nonejda@gmail.com>
2024-08-16 06:37:26 -0500

    1.0.0

--- __main__.py
+++ __main__.py
@@ -5,16 +5,19 @@ import matplotlib.pyplot as plt
 celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
+
 hidden_1 = tf.keras.layers.Dense(units=3, input_shape=[1])
 hidden_2 = tf.keras.layers.Dense(units=3)
 output = tf.keras.layers.Dense(units=1)
 
 model = tf.keras.Sequential([hidden_1, hidden_2, output])
 
+
 model.compile(
     optimizer=tf.keras.optimizers.Adam(0.1),
     loss='mean_squared_error'
 )
+
 
 print("Starting training...")
 history = model.fit(celsius, fahrenheit, epochs=1000, verbose=False)
@@ -26,9 +29,9 @@ plt.ylabel("Magnitude of loss")
 plt.plot(history.history["loss"])
 plt.show()
 
-
 print("Let's make a prediction!")
-result = model.predict([100.0])
+
+result = model.predict(np.array([100.0]))
 print(f"The result is {result[0][0]:.2f} fahrenheit!")
 
 print("Internal variables of the model")
